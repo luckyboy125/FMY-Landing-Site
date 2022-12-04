b549591989ebe7c0cb9b169a6d8f0b71acb9a660
luckyboy125 <luckyboy.ws125@gmail.com>
2022-12-04 16:53:02 +0200

    start structure of project

---
+++ src/components/WebAnalysisItems.js
@@ -0,0 +1,13 @@
+
+//Write your code here, if needed, create more componnents
+
+function WebAnalysisItems() {
+
+    return (
+        <div>
+            <h1 style={{color: "white"}}>Lorem Ipsum - Delete this line and write your code here</h1>
+        </div>
+    );
+}
+
+export default WebAnalysisItems;
