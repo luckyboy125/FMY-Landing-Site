72506acc6506d45cb7a508a20e65b9e0683f1378
luckyboy125 <117144469+luckyboy125@users.noreply.github.com>
2023-01-16 09:53:05 +0200

    added word cloud component in coparision page

--- src/pages/Cases/Cases.js
+++ src/pages/Cases/Cases.js
@@ -1,12 +1,12 @@
 import { useState } from "react";
 import { useLocation, useNavigate } from "react-router-dom";
+import BubbleChart from "./component/BubbleChart/BubbleChart";
+import Service from "./component/Service/Service";
+import Comparison from "./component/Comparison/Comparison";
 import ActionTab from "../../components/ActionTab/ActionTab";
 import PlusButton from "../../components/PlusButton/PlusButton";
 import SearchInput from "../../components/SearchInput/SearchInput";
-import BubbleChart from "./component/BubbleChart/BubbleChart";
-import Service from "./component/Service/Service";
 import "./Cases.css";
-import Comparison from "./component/Comparison/Comparison";
 
 function Cases() {
   const location = useLocation();
