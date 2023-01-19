2193f060e9a3b3507e167afd51182b415d2d6860
luckyboy125 <117144469+luckyboy125@users.noreply.github.com>
2023-01-19 10:06:13 +0200

    built search input component in investigation page

---
+++ src/components/DropdownLayout/DropdownLayout.js
@@ -0,0 +1,47 @@
+import { useEffect, useRef, useState } from "react";
+import "./DropdownLayout.css";
+
+function DropdownLayout({ className, children, dropRoot, dropRootStyle }) {
+  const [dropShow, setDropShow] = useState(false);
+  const contentRef = useRef(null);
+  const rootRef = useRef(null);
+
+  useEffect(() => {
+    if (dropShow) {
+      document.body.style.overflow = "hidden";
+    } else {
+      document.body.style.overflow = "auto";
+    }
+    document.addEventListener("click", handleClick);
+    return () => document.removeEventListener("click", handleClick);
+    function handleClick(e) {
+      if (contentRef && contentRef.current & rootRef && rootRef.current) {
+        const content = contentRef.current;
+        const root = rootRef.current;
+        if (!content.contains(e.target) && !root.contains(e.target)) {
+          setDropShow(false);
+        }
+      }
+    }
+  }, [contentRef, dropShow]);
+  console.log("show : ", dropShow);
+
+  return (
+    <div
+      className={`dropdownLayoutRoot ${className}`}
+      onClick={() => setDropShow(!dropShow)}
+      ref={rootRef}
+    >
+      {children}
+      {dropShow ? (
+        <div className={`dropRoot ${dropRootStyle}`} ref={contentRef}>
+          {dropRoot}
+        </div>
+      ) : (
+        <></>
+      )}
+    </div>
+  );
+}
+
+export default DropdownLayout;
--- src/pages/Investigation/Investigation.js
+++ src/pages/Investigation/Investigation.js
@@ -1,13 +1,18 @@
 import { useLocation, useNavigate } from "react-router-dom";
 import ActionTab from "../../components/ActionTab/ActionTab";
 import DatabaseSearchDropdown from "../../components/DatabaseSearchDropdown/DatabaseSearchDropdown";
+import searchIcon from "../../asset/images/search_icon_white.svg";
+import _searchIcon from "../../asset/images/search_icon.svg";
 import "./Investigation.css";
+import { useState } from "react";
+import DropdownLayout from "../../components/DropdownLayout/DropdownLayout";
 
 function Investigation() {
   const location = useLocation();
   const navigate = useNavigate();
   const query = new URLSearchParams(location.search);
   const tabData = ["All", "Lorem", "Lorem", "Lorem", "Lorem"];
+  const [searchUsername, setSearchUsername] = useState("");
 
   const handleTab = (_tab) => {
     query.set("investigation_tab", _tab);
@@ -43,7 +48,34 @@ function Investigation() {
               className="dropdown"
               type="keyword"
             />
-            <div className="inputRoot"></div>
+            <div className="inputRoot">
+              <img src={searchIcon} alt="search_icon" />
+              <input
+                placeholder="Enter username..."
+                value={searchUsername}
+                onChange={(e) => setSearchUsername(e.target.value)}
+              />
+              <div className="divline"></div>
+              <DropdownLayout
+                className="searchDropdown"
+                dropRoot={
+                  <div className="dropRoot">
+                    <div className="dropItem">Lorem ipsum</div>
+                    <div className="dropItem">Lorem ipsum</div>
+                    <div className="dropItem">Lorem ipsum</div>
+                    <div className="dropItem">Lorem ipsum</div>
+                    <div className="topRightArrow"></div>
+                  </div>
+                }
+                dropRootStyle="dropdownRoot"
+              >
+                Last 15 Days <i className="fas fa-caret-down"></i>
+              </DropdownLayout>
+              <div className="divline"></div>
+              <div className="searchBtn">
+                <img src={_searchIcon} alt="search_icon" />
+              </div>
+            </div>
           </div>
         </div>
       </div>
