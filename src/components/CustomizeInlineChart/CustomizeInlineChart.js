4ad2278a53b6c7850532399e25e2a9fce5d2e4af
luckyboy125 <117144469+luckyboy125@users.noreply.github.com>
2023-01-29 17:09:46 +0200

    added pressable function in inlinechart

--- src/components/CustomizeInlineChart/CustomizeInlineChart.js
+++ src/components/CustomizeInlineChart/CustomizeInlineChart.js
@@ -1,16 +1,34 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import "./CustomizeInlineChart.css";
 
 function CustomizeInlineChart() {
-  const data = [
+  const weekData = [
     "Monday",
     "Tuesday",
     "Wednesday",
     "Thursday",
-    "Today",
+    "Friday",
     "Saturday",
     "Sunday",
   ];
+
+  const [data, setData] = useState([
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+    "Today",
+  ]);
+
+  const handleClick = (index) => {
+    const array = [];
+    weekData.map((item, i) => {
+      i === index ? array.push("Today") : array.push(item);
+    });
+    setData(array);
+  };
   return (
     <>
       <div className="customizeInlineChartRoot">
@@ -23,6 +41,7 @@ function CustomizeInlineChart() {
                 left: `calc(${(100 / (data.length - 1)) * index}% - 63.5px)`,
                 top: "-7px",
               }}
+              onClick={() => handleClick(index)}
             >
               <div className="customizeInlineChartDot"></div>
               <span
