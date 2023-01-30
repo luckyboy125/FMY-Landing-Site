3b41f658b3a55cda7b9add68546de8b7f02db738
luckyboy125 <117144469+luckyboy125@users.noreply.github.com>
2023-01-30 18:58:08 +0200

    componentlized home page

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,69 +1,15 @@
 import { useState } from "react";
-import { useLocation, useNavigate } from "react-router-dom";
-import { useOutsideClick } from "../../hook/DetectOutsideClick";
-import HomeCard from "./component/HomeCard";
+import HomeCard from "./component/HomeCard/HomeCard";
+import NewUpdateModal from "./component/NewUpdateModal/NewUpdateModal";
 import PlusButton from "../../components/PlusButton/PlusButton";
 import ActionButton from "../../components/ActionButton/ActionButton";
-import ModalLayout from "../../components/ModalLayout/ModalLayout";
 import CustomizeInlineChart from "../../components/CustomizeInlineChart/CustomizeInlineChart";
 import handImage from "../../asset/images/hand.svg";
-import CloseIcon from "../../asset/images/close_icon.svg";
 import "./Home.css";
 
 function Home() {
-  const location = useLocation();
-  const navigate = useNavigate();
-  const query = new URLSearchParams(location.search);
-
   const compare = ["This Week", "Custom"];
   const [modalShow, setModalShow] = useState(false);
-  const [categoryValue, setCategoryValue] = useState("");
-  const [categorySelectShow, setCategorySelectShow] = useState(false);
-  const categorySelectRef = useOutsideClick(() => setCategorySelectShow(false));
-  const categorySelect = [
-    "News update",
-    "Event",
-    "Human rights",
-    "Lorem ipsum",
-    "Lorem ipsum",
-    "Lorem ipsum",
-    "Lorem ipsum",
-  ];
-
-  const handlCategory = (category) => {
-    query.set("newupdate_category", category);
-    navigate({
-      pathname: location.pathname,
-      search: query.toString(),
-    });
-    setCategorySelectShow(false);
-  };
-
-  const [caseValue, setCaseValue] = useState("");
-
-  const [caseSelectShow, setCaseSelectShow] = useState(false);
-  const caseSelectRef = useOutsideClick(() => setCaseSelectShow(false));
-  const caseSelect = [
-    "Lorem ipsum",
-    "Lorem ipsum",
-    "Human rights",
-    "Lorem ipsum",
-    "Lorem ipsum",
-    "Lorem ipsum",
-    "Lorem ipsum",
-  ];
-
-  const handlCase = (item) => {
-    query.set("newupdate_case", item);
-    navigate({
-      pathname: location.pathname,
-      search: query.toString(),
-    });
-    setCaseSelectShow(false);
-  };
-
-  const [updateValue, setUpdateValue] = useState("");
-  const [attachLinkValue, setAttachLinkValue] = useState("");
 
   return (
     <div className="homeRoot">
@@ -79,6 +25,7 @@ function Home() {
           action={() => setModalShow(true)}
           className="homeBtn"
         />
+        <NewUpdateModal show={modalShow} onClose={() => setModalShow(false)} />
       </div>
       <div className="lineRoot">
         <ActionButton
@@ -150,119 +97,6 @@ function Home() {
           />
         </div>
       </div>
-      <ModalLayout
-        show={modalShow}
-        onClose={() => setModalShow(false)}
-        className="newUpdataModalRoot"
-      >
-        <img
-          src={CloseIcon}
-          className="closeIcon"
-          alt="closeIcon"
-          onClick={() => setModalShow(false)}
-        />
-        <div className="modalTitle">New update</div>
-        <div className="modalInputContent">
-          <div className="modalItem">
-            <div className="title">Category</div>
-            <div className="inputRoot">
-              <input
-                value={categoryValue}
-                placeholder="Write update here"
-                onChange={(e) => setCategoryValue(e.target.value)}
-              />
-              <div className="plus">+</div>
-              <div
-                className="roundBtn"
-                onClick={() => setCategorySelectShow(!categorySelectShow)}
-              >
-                <i className="fas fa-chevron-down" />
-                {categorySelectShow ? (
-                  <div className="selectDropdownRoot" ref={categorySelectRef}>
-                    {categorySelect.map((item, index) => {
-                      return (
-                        <div
-                          key={index}
-                          className={`${
-                            query.get("newupdate_category") === item
-                              ? "activeItem"
-                              : "item"
-                          }`}
-                          onClick={() => handlCategory(item)}
-                        >
-                          {item}
-                        </div>
-                      );
-                    })}
-                  </div>
-                ) : (
-                  <></>
-                )}
-              </div>
-            </div>
-          </div>
-          <div className="modalItem">
-            <div className="title">Cases</div>
-            <div className="inputRoot">
-              <input
-                value={caseValue}
-                placeholder="Choose case"
-                onChange={(e) => setCaseValue(e.target.value)}
-              />
-              <div className="plus">+</div>
-              <div
-                className="roundBtn"
-                onClick={() => setCaseSelectShow(!caseSelectShow)}
-              >
-                <i className="fas fa-chevron-down" />
-                {caseSelectShow ? (
-                  <div className="selectDropdownRoot" ref={caseSelectRef}>
-                    {caseSelect.map((item, index) => {
-                      return (
-                        <div
-                          key={index}
-                          className={`${
-                            query.get("newupdate_case") === item
-                              ? "activeItem"
-                              : "item"
-                          }`}
-                          onClick={() => handlCase(item)}
-                        >
-                          {item}
-                        </div>
-                      );
-                    })}
-                  </div>
-                ) : (
-                  <></>
-                )}
-              </div>
-            </div>
-          </div>
-          <div className="modalItem">
-            <div className="title">Update</div>
-            <div className="inputRoot">
-              <input
-                value={updateValue}
-                placeholder="Write update here"
-                onChange={(e) => setUpdateValue(e.target.value)}
-              />
-            </div>
-          </div>
-          <div className="modalItem">
-            <div className="title">Attach link</div>
-            <div className="inputRoot">
-              <input
-                value={attachLinkValue}
-                placeholder="Paste link here..."
-                onChange={(e) => setAttachLinkValue(e.target.value)}
-              />
-            </div>
-          </div>
-        </div>
-        <div className="addLink">+ Add another link</div>
-        <div className="updateBtn">Update</div>
-      </ModalLayout>
     </div>
   );
 }
---
+++ src/pages/Home/component/NewUpdateModal/NewUpdateModal.js
@@ -0,0 +1,174 @@
+import { useState } from "react";
+import { useLocation, useNavigate } from "react-router-dom";
+import { useOutsideClick } from "../../../../hook/DetectOutsideClick";
+import CloseIcon from "../../../../asset/images/close_icon.svg";
+import ModalLayout from "../../../../components/ModalLayout/ModalLayout";
+import "./NewUpdateModal.css";
+
+function NewUpdateModal({ show, onClose }) {
+  const location = useLocation();
+  const navigate = useNavigate();
+  const query = new URLSearchParams(location.search);
+
+  const [categoryValue, setCategoryValue] = useState("");
+  const [categorySelectShow, setCategorySelectShow] = useState(false);
+  const categorySelectRef = useOutsideClick(() => setCategorySelectShow(false));
+  const categorySelect = [
+    "News update",
+    "Event",
+    "Human rights",
+    "Lorem ipsum",
+    "Lorem ipsum",
+    "Lorem ipsum",
+    "Lorem ipsum",
+  ];
+
+  const handlCategory = (category) => {
+    query.set("newupdate_category", category);
+    navigate({
+      pathname: location.pathname,
+      search: query.toString(),
+    });
+    setCategorySelectShow(false);
+  };
+
+  const [caseValue, setCaseValue] = useState("");
+
+  const [caseSelectShow, setCaseSelectShow] = useState(false);
+  const caseSelectRef = useOutsideClick(() => setCaseSelectShow(false));
+  const caseSelect = [
+    "Lorem ipsum",
+    "Lorem ipsum",
+    "Human rights",
+    "Lorem ipsum",
+    "Lorem ipsum",
+    "Lorem ipsum",
+    "Lorem ipsum",
+  ];
+
+  const handlCase = (item) => {
+    query.set("newupdate_case", item);
+    navigate({
+      pathname: location.pathname,
+      search: query.toString(),
+    });
+    setCaseSelectShow(false);
+  };
+
+  const [updateValue, setUpdateValue] = useState("");
+  const [attachLinkValue, setAttachLinkValue] = useState("");
+
+  return (
+    <ModalLayout show={show} onClose={onClose} className="newUpdataModalRoot">
+      <img
+        src={CloseIcon}
+        className="closeIcon"
+        alt="closeIcon"
+        onClick={onClose}
+      />
+      <div className="modalTitle">New update</div>
+      <div className="modalInputContent">
+        <div className="modalItem">
+          <div className="title">Category</div>
+          <div className="inputRoot">
+            <input
+              value={categoryValue}
+              placeholder="Write update here"
+              onChange={(e) => setCategoryValue(e.target.value)}
+            />
+            <div className="plus">+</div>
+            <div
+              className="roundBtn"
+              onClick={() => setCategorySelectShow(!categorySelectShow)}
+            >
+              <i className="fas fa-chevron-down" />
+              {categorySelectShow ? (
+                <div className="selectDropdownRoot" ref={categorySelectRef}>
+                  {categorySelect.map((item, index) => {
+                    return (
+                      <div
+                        key={index}
+                        className={`${
+                          query.get("newupdate_category") === item
+                            ? "activeItem"
+                            : "item"
+                        }`}
+                        onClick={() => handlCategory(item)}
+                      >
+                        {item}
+                      </div>
+                    );
+                  })}
+                </div>
+              ) : (
+                <></>
+              )}
+            </div>
+          </div>
+        </div>
+        <div className="modalItem">
+          <div className="title">Cases</div>
+          <div className="inputRoot">
+            <input
+              value={caseValue}
+              placeholder="Choose case"
+              onChange={(e) => setCaseValue(e.target.value)}
+            />
+            <div className="plus">+</div>
+            <div
+              className="roundBtn"
+              onClick={() => setCaseSelectShow(!caseSelectShow)}
+            >
+              <i className="fas fa-chevron-down" />
+              {caseSelectShow ? (
+                <div className="selectDropdownRoot" ref={caseSelectRef}>
+                  {caseSelect.map((item, index) => {
+                    return (
+                      <div
+                        key={index}
+                        className={`${
+                          query.get("newupdate_case") === item
+                            ? "activeItem"
+                            : "item"
+                        }`}
+                        onClick={() => handlCase(item)}
+                      >
+                        {item}
+                      </div>
+                    );
+                  })}
+                </div>
+              ) : (
+                <></>
+              )}
+            </div>
+          </div>
+        </div>
+        <div className="modalItem">
+          <div className="title">Update</div>
+          <div className="inputRoot">
+            <input
+              value={updateValue}
+              placeholder="Write update here"
+              onChange={(e) => setUpdateValue(e.target.value)}
+            />
+          </div>
+        </div>
+        <div className="modalItem">
+          <div className="title">Attach link</div>
+          <div className="inputRoot">
+            <input
+              value={attachLinkValue}
+              placeholder="Paste link here..."
+              onChange={(e) => setAttachLinkValue(e.target.value)}
+            />
+          </div>
+        </div>
+      </div>
+      <div className="addLink">+ Add another link</div>
+      <div className="updateBtn">Update</div>
+    </ModalLayout>
+  );
+}
+
+export default NewUpdateModal;
